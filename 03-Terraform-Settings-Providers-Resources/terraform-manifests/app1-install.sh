13ef5f1a2fc84419e4b3cc1d216e2e0267ea7803
ujwalapotluri <potluriujwala04@gmail.com>
2025-03-11 17:14:48 +0530

    Update app1-install.sh

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/app1-install.sh
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/app1-install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
-sudo yum update -y
-sudo yum install -y httpd
+sudo apt update -y
+sudo apt install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start  
 sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
